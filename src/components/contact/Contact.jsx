57e618ab9a3000479c608fe9b917b94a5330c0a7
Kalash Kumari Thakur <152004250+Kkt04@users.noreply.github.com>
2024-09-23 19:29:22 +0530

    b

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Back from "../common/back/Back";
 import "./contact.css";
 
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade';
+  const [messageSent, setMessageSent] = useState(false);
+  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessageSent(true); 
+  };
 
   return (
     <>
@@ -11,7 +17,7 @@ const Contact = () => {
       <section className='contacts padding'>
         <div className='container shadow flexSB'>
           <div className='left row'>
-            <iframe src={map} frameborder="0" allowfullscreen></iframe>
+            <iframe src={map} frameBorder="0" allowFullScreen></iframe>
           </div>
           <div className='right row'>
             <h1>Contact us</h1>
@@ -24,7 +30,7 @@ const Contact = () => {
               </div>
               <div className='box'>
                 <h4>EMAIL:</h4>
-                <p> coursebazar@yoursite.com</p>
+                <p>coursebazar@yoursite.com</p>
               </div>
               <div className='box'>
                 <h4>PHONE:</h4>
@@ -32,19 +38,17 @@ const Contact = () => {
               </div>
             </div>
 
-            <form action=''>
-              <div className='flexSB'>
-                <input type='text' placeholder='Name' />
-                <input type='email' placeholder='Email' />
-              </div>
-              <input type='text' placeholder='Subject' />
-              <textarea cols='30' rows='10'>
-                Create a message here...
-              </textarea>
-              <button className='primary-btn'>SEND MESSAGE</button>
-            </form>
+            {messageSent && <p className='success-message'>Your message has been sent!</p>} 
 
-            
+            <form onSubmit={handleSubmit}>
+              <div className='flexSB'>
+                <input type='text' placeholder='Name' required />
+                <input type='email' placeholder='Email' required />
+              </div>
+              <input type='text' placeholder='Subject' required />
+              <textarea cols='30' rows='10' placeholder='Create a message here...' required></textarea>
+              <button type='submit' className='primary-btn'>SEND MESSAGE</button>
+            </form>
           </div>
         </div>
       </section>
