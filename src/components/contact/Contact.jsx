35e212fb155410be0727afcc6ca072c285b412a8
Kalash Kumari Thakur <152004250+Kkt04@users.noreply.github.com>
2024-09-24 19:05:12 +0530

    k

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,6 +15,7 @@ const Contact = () => {
     <>
       <Back title='Contact us' />
       <section className='contacts padding'>
+        <div className='scroll-down-icon'></div> 
         <div className='container shadow flexSB'>
           <div className='left row'>
             <iframe src={map} frameBorder="0" allowFullScreen></iframe>
@@ -53,7 +54,7 @@ const Contact = () => {
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default Contact;
